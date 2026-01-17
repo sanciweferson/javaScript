4d4183478b80b320d9d0d57d231a55eb2e101912
Sanciweferson <san@san.localdomain>
2026-01-16 23:58:24 -0300

    estrutura inicial var

--- javascript/var/index.js
+++ javascript/var/index.js
@@ -1,3 +1,16 @@
 var varDeclarado = document.getElementById("js-value-1")
 var name = "Sanciweferson"
 varDeclarado.textContent = `${name}`
+
+
+
+var varRedeclarado = document.getElementById("js-value-2")
+var name = "San"
+varRedeclarado.textContent = `${name}`
+
+
+
+var varReatribuido = document.getElementById("js-value-3")
+var name = "Weferson"
+
+varReatribuido.textContent = `${name}`
